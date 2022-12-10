2312a11582d42a2a8e68177e38a7778897b38054
GiAn <gian.gavins@gmail.com>
2022-12-10 17:28:11 -0500

    Update description section

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -5,8 +5,11 @@ const Description = () => {
     <React.Fragment>
       <h2>1.- Descripción MamerApp</h2>
       <p>MamerApp, S.L. (“Mamer app” o el “TITULAR”) es la titular de la aplicación móvil MamerApp (“en adelante la APP”). La APP ayuda a las personas de la probabilidad de sufrir soledad y pone a disposición de los USUARIOS (en adelante, individualmente, el “USUARIO”, y conjuntamente los “USUARIOS”), la posibilidad de navegar por la Aplicación, accediendo a los contenidos, de acuerdo con las presentes CONDICIONES DE USO, con el objetivo de ayudar a la población en relación con la información o mensajes que envía un personaje ficticio llamado Mamer.
+      </p>
+      
+      <p>AVISO IMPORTANTE: El USUARIO queda avisado de que la utilización de la APP NO CONSTITUYE EN NINGÚN CASO UN SERVICIO DE AYUDA MÉDICA, PSICOLÓGICA, DE ATENCIÓN DE NINGÚN TIPO SOLO ES ESTRICTAMENTE DE ENTRENTENIMIENTO.</p>
 
-      ‍AVISO IMPORTANTE: El USUARIO queda avisado de que la utilización de la APP NO CONSTITUYE EN NINGÚN CASO UN SERVICIO DE AYUDA MÉDICA, PSICOLÓGICA, DE ATENCIÓN DE NINGÚN TIPO SOLO ES ESTRICTAMENTE DE ENTRENTENIMIENTO. La utilización de la APP no puede, en ningún caso, sustituir la consulta presencial personal frente a un profesional a amigo o consejero profesional Mamer es solo una persona tonta que da consejos tontos con el propósito de divertir y/o entretener.</p>
+      <p> La utilización de la APP no puede, en ningún caso, sustituir la consulta presencial personal frente a un profesional, amigo o consejero profesional. Mamer es solo una personaje tonto que da consejos tontos por no decir inútiles con el propósito de divertir y/o entretener.</p>
     </React.Fragment>
   )
 }
